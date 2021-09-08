116dc48a842948d91eedba33d76c2165259369d9
Patrick Lehner <lehner.pa@gmail.com>
2021-09-08 15:52:15 +0200

    Add basic checking of text via LT API from Word Add-In

--- src/word-taskpane/TaskpaneApp.tsx
+++ src/word-taskpane/TaskpaneApp.tsx
@@ -1,33 +1,50 @@
-import React, { FC } from "react";
+import React, { Dispatch, FC, SetStateAction, useState } from "react";
 import styled from "styled-components";
 import { DefaultButton } from "@fluentui/react";
+import { RuleMatch } from "../common/language-tool-api/types";
+import { ResultsArea } from "../common/results-display/ResultsArea";
+import { LanguageToolClient } from "../common/language-tool-api/LanguageToolClient";
 
-export const TaskpaneApp: FC = () => (
-  <div>
-    <TaskpaneHeading>Taskpane Page</TaskpaneHeading>
-    <p>This page is the TASKPANE entrypoint2</p>
-    <DefaultButton className="ms-welcome__action" iconProps={{ iconName: "ChevronRight" }} onClick={clickHandler}>
-      Run
-    </DefaultButton>
-  </div>
-);
+export const TaskpaneApp: FC = () => {
+  const [ltMatches, setLtMatches] = useState<RuleMatch[]>([]);
+  const [isLoading, setLoading] = useState(false);
+
+  return (
+    <div>
+      <TaskpaneHeading>OpenMinDEd</TaskpaneHeading>
+      <DefaultButton
+        className="ms-welcome__action"
+        iconProps={{ iconName: "ChevronRight" }}
+        onClick={async () => {
+          setLoading(true);
+          await clickHandler(setLtMatches);
+          setLoading(false);
+        }}
+      >
+        Prüfen
+      </DefaultButton>
+
+      {isLoading ? <div>Loading...</div> : <ResultsArea ruleMatches={ltMatches} />}
+    </div>
+  );
+};
 
 const TaskpaneHeading = styled.h1`
   color: darkgreen;
 `;
 
-const clickHandler = async () => {
-  return Word.run(async (context) => {
-    /**
-     * Insert your Word code here
-     */
-
-    // insert a paragraph at the end of the document.
-    const paragraph = context.document.body.insertParagraph("Hello World", Word.InsertLocation.end);
-
-    // change the paragraph color to blue.
-    paragraph.font.color = "blue";
-
+const clickHandler = async (setLtMatches: Dispatch<SetStateAction<RuleMatch[]>>) => {
+  let text: string = "";
+  await Word.run(async (context) => {
+    const range = context.document.body.getRange(Word.RangeLocation.content);
+    range.load();
     await context.sync();
+    text = range.text;
   });
+  const request = {
+    text,
+    language: "auto",
+  };
+  const content = await new LanguageToolClient().check(request);
+  setLtMatches(() => content.matches || []);
 };
