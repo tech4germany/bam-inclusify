3dd896370f00ed7414f9b93375900a908bca603c
Patrick Lehner <lehner.pa@gmail.com>
2021-09-17 13:55:14 +0200

    Intermediate state with POC code to get and update text based on single-word text ranges in Word

--- react-ui/src/word-taskpane/TaskpaneApp.tsx
+++ react-ui/src/word-taskpane/TaskpaneApp.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch, FC, SetStateAction, useState } from "react";
+import React, { Dispatch, FC, SetStateAction, useEffect, useState } from "react";
 import styled from "styled-components";
 import { DefaultButton } from "@fluentui/react";
 import { RuleMatch } from "../common/language-tool-api/types";
@@ -17,6 +17,16 @@ export const TaskpaneApp: FC = () => {
   // const [ltMatches, setLtMatches] = useState<RuleMatch[]>([]);
   const [checkResult, setCheckResult] = useState<CheckResult>();
   const [isLoading, setLoading] = useState(false);
+  const [ranges, setRanges] = useState<Word.Range[]>([]);
+
+  // useEffect(() => {
+  //   const doc = Office.context.document;
+  //   doc.addHandlerAsync(Office.EventType.DocumentSelectionChanged, () => {
+  //     doc.getSelectedDataAsync(Office.CoercionType.Text, (r) => {
+  //       console.log("Selection changed", r.value);
+  //     });
+  //   });
+  // });
 
   return (
     <div>
@@ -37,11 +47,23 @@ export const TaskpaneApp: FC = () => {
           className="ms-welcome__action"
           onClick={async () => {
             setLoading(true);
-            await debugClickHandler();
+            await debugClickHandler(setRanges);
             setLoading(false);
           }}
         >
           Debug
+        </DefaultButton>
+        <DefaultButton
+          className="ms-welcome__action"
+          onClick={async () => {
+            await Word.run(async (context) => {
+              const r = ranges[0];
+              r.insertText("Replaced", Word.InsertLocation.replace);
+              await r.context.sync();
+            });
+          }}
+        >
+          Debug2
         </DefaultButton>
         {!!checkResult && (
           <DefaultButton
@@ -88,37 +110,38 @@ const clickHandler = async (setLtMatches: (newCheckResult: CheckResult) => void)
   //   text,
   //   language: "auto",
   // };
-  let structuredText: string = "";
-  if (isRunningInWord()) {
-    await Word.run(async (context) => {
-      const ooxml = context.document.body.getOoxml();
-      await context.sync();
-      structuredText = ooxml.value;
-    });
-  } else if (isRunningInOutlook()) {
-    const mailboxItem = Office.context.mailbox.item;
-    if (!mailboxItem) {
-      throw new Error("No mailbox item selected?");
-    }
-    structuredText = await new Promise((resolve, reject) => {
-      mailboxItem.body.getAsync(Office.CoercionType.Html, (result) => resolve(result.value));
-    });
-  } else {
-    throw new Error("Unknown office host app");
-  }
+  if (!isRunningInWord()) throw new Error("Unknown office host app");
 
-  const { doc, startIndexMap } = findTextNodesInXml(structuredText);
-  const plaintext = startIndexMap.map((t) => t.textNode.textContent!).join("");
+  console.time("getTextRanges");
+  const textRanges = await Word.run(async (context) => {
+    const paragraphs = context.document.body.paragraphs.track().load();
+    await context.sync();
+    const ranges = paragraphs.items.map((p) =>
+      p.getRange(Word.RangeLocation.whole).getTextRanges([" "], false).track().load()
+    );
+    // ranges.load();
+    await context.sync();
+    const textRangeCount = ranges.reduce((acc, curr) => acc + curr.items.length, 0);
+    console.log(`Got ${ranges.length} paragraphs with overall ${textRangeCount} text ranges`);
 
-  const request = {
-    text: plaintext,
-    language: "auto",
-  };
-  const content = await new LanguageToolClient().check(request);
-  setLtMatches({ doc, startIndexMap, ruleMatches: content.matches || [] });
+    return ranges;
+  });
+
+  const plaintext = textRanges.map((p) => p.items.map((r) => r.text).join("")).join("\n");
+  console.log(`Have plaintext: `, plaintext);
+  console.timeEnd("getTextRanges");
+  // const { doc, startIndexMap } = findTextNodesInXml(structuredText);
+  // const plaintext = startIndexMap.map((t) => t.textNode.textContent!).join("");
+
+  // const request = {
+  //   text: plaintext,
+  //   language: "auto",
+  // };
+  // const content = await new LanguageToolClient().check(request);
+  // setLtMatches({ doc, startIndexMap, ruleMatches: content.matches || [] });
 };
 
-const debugClickHandler = async () => {
+const debugClickHandler = async (setParagraphs: (newParagraphs: Word.Range[]) => void) => {
   // console.log(getOfficeHostInfo());
   // return;
   await Word.run(async (context) => {
@@ -131,23 +154,27 @@ const debugClickHandler = async () => {
     // tr.items[1].insertText("Englische ", Word.InsertLocation.replace);
     // await context.sync();
 
-    const paragraphs = context.document.body.paragraphs;
-    // paragraphs.load();
-    // await context.sync();
+    const ranges = context.document.body.getRange(Word.RangeLocation.whole).getTextRanges([" "], false).track();
+    ranges.load();
+    await context.sync();
 
-    const p1 = paragraphs.getFirst();
-    const range = context.document.body.getRange(Word.RangeLocation.content);
-    range.load();
-    await context.sync();
-    const oox = range.getOoxml();
-    // const oox = p1.getOoxml();
-    await context.sync();
+    const newRanges = ranges.items;
+    setParagraphs(newRanges);
+    console.log(newRanges);
+
+    // const p1 = paragraphs.getFirst();
+    // const range = context.document.body.getRange(Word.RangeLocation.content);
+    // range.load();
+    // await context.sync();
+    // const oox = range.getOoxml();
+    // // const oox = p1.getOoxml();
+    // await context.sync();
     // console.log(oox.value);
 
-    const parser = new DOMParser();
-    const dom = parser.parseFromString(oox.value, "application/xml");
-    debugger;
-    console.log(dom);
+    // const parser = new DOMParser();
+    // const dom = parser.parseFromString(oox.value, "application/xml");
+    // debugger;
+    // console.log(dom);
     ///package/part[2]/xmlData/document/body/p[1]/r[1]/t
     // console.log(
     //   dom.evaluate("/package/part[2]/xmlData/document/body/p[1]/r[1]/t", dom, null, XPathResult.FIRST_ORDERED_NODE_TYPE)
