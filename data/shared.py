c0ea9c41bec834ecbaedb035adf8d048236a8367
David Pomerenke <david.pomerenke@uni-konstanz.de>
2021-09-28 21:28:38 +0200

    Flatten directory structure

--- data/shared.py
+++ data/shared.py
@@ -26,9 +26,9 @@ def dict_to_csvs(dic: Dict[str, Dict[str, List[str]]], name: str) -> None:
         df.to_csv("{}_{}.csv".format(name, n), header=False)
 
 
-def csvs_to_dict(name: str) -> Dict[str, Dict[str, List[str]]]:
+def csvs_to_dict(name: str, numbers: List[str] = ["sg", "pl"]) -> Dict[str, Dict[str, List[str]]]:
     dic: Dict[str, Dict[str, List[str]]] = {"sg": {}, "pl": {}}
-    for n in ["sg", "pl"]:
+    for n in numbers:
         df = pd.read_csv("{}_{}.csv".format(name, n), header=None)
         d = df.set_index(0).T.to_dict("list")
         d = dict(
