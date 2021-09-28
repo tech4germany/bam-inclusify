6d43a585d0d31a66792da8c64248e40da8936025
David Pomerenke <david.pomerenke@uni-konstanz.de>
2021-09-28 20:07:33 +0200

    Extra script for Vienna catalog with proper documentation

--- data/shared.py
+++ data/shared.py
@@ -1,8 +1,8 @@
 from typing import *
+import pandas as pd
 
-A = NewType("A", Any)
-
-B = NewType("B", Any)
+A = TypeVar("A")
+B = TypeVar("B")
 
 
 def log(a: str, x: A) -> A:
@@ -18,3 +18,24 @@ def add_to_dict(key: A, vals: List[B], dic: Dict[A, List[B]]) -> Dict[A, List[B]
                 dic[key].append(val)
     else:
         dic[key] = vals
+
+
+def dict_to_csvs(dic: Dict[str, Dict[str, List[str]]], name: str) -> None:
+    for n in ["sg", "pl"]:
+        df = pd.DataFrame.from_dict(dic[n], orient="index")
+        df.to_csv("{}_{}.csv".format(name, n), header=False)
+
+
+def csvs_to_dict(name: str) -> Dict[str, Dict[str, List[str]]]:
+    dic: Dict[str, Dict[str, List[str]]] = {"sg": {}, "pl": {}}
+    for n in ["sg", "pl"]:
+        df = pd.read_csv("{}_{}.csv".format(name, n), header=None)
+        d = df.set_index(0).T.to_dict("list")
+        d = dict(
+            [
+                (key, [val for val in vals if type(val) == str])
+                for key, vals in d.items()
+            ]
+        )
+        dic[n] = d
+    return dic
