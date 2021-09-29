5eda0520769d6f8f829ac8711abad08ed38b7dbf
David Pomerenke <david.pomerenke@uni-konstanz.de>
2021-09-29 09:58:10 +0200

    Refactor main rule processing script

--- data/copy_files.py
+++ data/copy_files.py
@@ -2,32 +2,38 @@
 # -*- coding: utf-8 -*-
 from os import path
 
+# adjust this to the folder of the LanguageTool release
+languagetool_path = path.join("..", "languagetool", "LanguageTool-5.4")
+# path of the German grammar files within the LanguageTool release
+grammar_path = path.join(languagetool_path, "org", "languagetool", "rules", "de")
+
 def copy_files():
-  languagetool_path = path.join("..", "languagetool", "LanguageTool-5.4") # adjust this to the folder of the LanguageTool release
-  grammar_path = path.join(languagetool_path, "org", "languagetool", "rules", "de") # path of the German grammar files within the LanguageTool release
+    xml = open("grammar_openminded.xml").read()
+    custom_filename = "grammar_openminded.xml"
+    open(custom_filename, "w").write(xml)
+    open(path.join(grammar_path, custom_filename), "w").write(xml)
 
-  xml = open(path.join("wordlists", "grammar_custom.xml")).read()
-  custom_filename = "grammar_custom.xml"
-  open(path.join("wordlists", custom_filename), "w").write(xml)
-  open(path.join(grammar_path, custom_filename), "w").write(xml)
+    # Use backup file if available (see comments below)
+    if path.isfile(path.join(grammar_path, "grammar.xml.old")):
+        old_xml = open(path.join(grammar_path, "grammar.xml.old")).read()
+    else:
+        old_xml = open(path.join(grammar_path, "grammar.xml")).read()
+        # Save backup of the old grammar file.
+        open(path.join(grammar_path, "grammar.xml.old"), "w").write(old_xml)
 
-  # Use backup file if available (see comments below)
-  if path.isfile(path.join(grammar_path, "grammar.xml.old")):
-    old_xml = open(path.join(grammar_path, "grammar.xml.old")).read()
-  else:
-    old_xml = open(path.join(grammar_path, "grammar.xml")).read()
-    # Save backup of the old grammar file.
-    open(path.join(grammar_path, "grammar.xml.old"), "w").write(old_xml)
-
-  new_xml = old_xml.replace(
-        "<!DOCTYPE rules [", 
-        '<!DOCTYPE rules [ \n\t<!ENTITY UserRules SYSTEM "file:///{}">'.format(path.abspath(path.join(grammar_path, custom_filename)))
+    new_xml = old_xml.replace(
+        "<!DOCTYPE rules [",
+        '<!DOCTYPE rules [ \n\t<!ENTITY UserRules SYSTEM "file:///{}">'.format(
+            path.abspath(path.join(grammar_path, custom_filename))
+        ),
     ).replace(
-        "</rules>", 
-        '<category id="DIVERSITY_SENSITIVE_LANGUAGE" name="Erweiterung für diversitätssensible Sprache">\n&UserRules;\n</category>\n</rules>'
+        "</rules>",
+        '<category id="DIVERSITY_SENSITIVE_LANGUAGE" name="Erweiterung für diversitätssensible Sprache">\n&UserRules;\n</category>\n</rules>',
     )
 
-  # Replace with file where the new rules have been added.
-  open(path.join(grammar_path, "grammar.xml"), "w").write(new_xml)
+    # Replace with file where the new rules have been added.
+    open(path.join(grammar_path, "grammar.xml"), "w").write(new_xml)
 
-copy_files()
+
+if __name__ == "__main__":
+    copy_files()
