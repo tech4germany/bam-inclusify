68295401b5eb9a306fae9f6c6d26221cf30313d9
David Pomerenke <david.pomerenke@uni-konstanz.de>
2021-09-29 15:55:30 +0200

    Add unzipping to devcmd script

--- dev_cmds/start-api.ts
+++ dev_cmds/start-api.ts
@@ -2,6 +2,12 @@ import { execPiped, runAsyncMain } from "devcmd";
 
 async function main() {
   await execPiped({
+    command: "unzip",
+    args: ["LanguageTool-5.4.zip"],
+    options: { cwd: "../languagetool" },
+  });
+
+  await execPiped({
     command: "python3",
     args: ["copy_files.py"],
     options: { cwd: "../data" },
