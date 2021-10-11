8d7bb38eadebd0324ac27422efc900e4ea2b4093
Patrick Lehner <lehner.pa@gmail.com>
2021-10-11 10:17:02 +0200

    Expand list of rule category IDs in frontend

--- react-ui/src/common/rule-categories.ts
+++ react-ui/src/common/rule-categories.ts
@@ -6,18 +6,32 @@ export function mapRuleCategory(ruleMatch: RuleMatch): RuleMatchCategory {
   return mapRuleCategoryId(ruleMatch.rule?.category.id);
 }
 
+const diversityRuleCategories = ["GENERISCHES_MASKULINUM", "BEHINDERUNG"];
+const spellingRuleCategories = [
+  "TYPOS",
+  "EMPFOHLENE_RECHTSCHREIBUNG",
+  "PROPER_NOUNS",
+  "CONFUSED_WORDS",
+  "COMPOUNDING",
+  "CORRESPONDENCE",
+  "CASING",
+];
+const grammarRuleCategories = [
+  "SEMANTICS",
+  "REDUNDANCY",
+  "COLLOQUIALISMS",
+  "STYLE",
+  "GRAMMAR",
+  "IDIOMS",
+  "PUNCTUATION",
+  "TYPOGRAPHY",
+  "HILFESTELLUNG_KOMMASETZUNG",
+];
+
 function mapRuleCategoryId(categoryId: string | undefined): RuleMatchCategory {
-  switch (categoryId) {
-    case "GENERISCHES_MASKULINUM":
-    case "BEHINDERUNG":
-      return "diversity";
-    case "TYPOS":
-      return "spelling";
-    case "REDUNDANCY":
-    case "GRAMMAR":
-    case "PUNCTUATION":
-      return "grammar";
-    default:
-      return "unknown";
-  }
+  if (categoryId === undefined) return "unknown";
+  if (diversityRuleCategories.includes(categoryId)) return "diversity";
+  if (spellingRuleCategories.includes(categoryId)) return "spelling";
+  if (grammarRuleCategories.includes(categoryId)) return "grammar";
+  return "unknown";
 }
