23347a962ae60e0f38dddc3a5836aca893f5ba03
David Pomerenke <david.pomerenke@uni-konstanz.de>
2021-10-12 16:11:15 +0200

    Reduce replaced words to base form

--- data/create_grammars.py
+++ data/create_grammars.py
@@ -1,9 +1,12 @@
+from copy import copy
 from create_xml import rule_to_xml
 from gender_config import gender_languages
 from helpers import add_to_dict, open_
 from helpers_csv import csvs_to_dict, dict_to_csvs
 from os import path
 from paths import *
+import re
+import subprocess
 import sys
 from typing import *
 sys.path.insert(0, "retext-equality")
@@ -11,7 +14,8 @@ from create_illness_rules import create_illness_rules
 
 
 def unified_dic() -> Dict[str, Dict[str, List[str]]]:
-    unified_dic: Dict[str, Dict[str, List[str]]] = {"sg": {}, "pl": {}, "combined": {}}
+    unified_dic: Dict[str, Dict[str, List[str]]] = {
+        "sg": {}, "pl": {}, "combined": {}}
 
     files = {
         "dereko/dereko_unified": ["sg", "pl"],
@@ -24,6 +28,8 @@ def unified_dic() -> Dict[str, Dict[str, List[str]]]:
     for file, numbers in files.items():
         dic[file] = csvs_to_dict(file, numbers=numbers)
         for n in numbers:
+            replacements = base_form_variations(dic[file][n].keys())
+            dic[file][n] = replace_keys(replacements, dic[file][n])
             print(file, n, len(dic[file][n]))
             for key, vals in dic[file][n].items():
                 add_to_dict(key, vals, unified_dic[n])
@@ -61,6 +67,31 @@ def check_xml() -> None:
     stderr = result.stderr.decode("UTF-8")
     open("rule_validation_stdout.log", "w").write(stdout)
     open("rule_validation_stderr.log", "w").write(stderr)
+
+
+def base_form_variations(inflected_forms: List[str]) -> List[Tuple[str, str]]:
+    open_(path.join(languagetool_build_path, "input.txt"), "w").write(" ".join(inflected_forms))
+    result = subprocess.run(
+        "java -jar languagetool-commandline.jar --taggeronly --language de-DE input.txt".split(" "), cwd=languagetool_build_path, capture_output=True
+    )
+    stdout = result.stdout.decode("UTF-8")
+    stderr = result.stderr.decode("UTF-8")
+    all_base_forms = re.findall(r"([A-ZÄÖÜa-zäöüß\-]+)\[([A-ZÄÖÜa-zäöüß\-]+)", stdout)
+    without_base_form = re.findall(r"([A-ZÄÖÜa-zäöüß\-]+)\[([A-ZÄÖÜa-zäöüß\-]+)/null", stdout)
+    return [(a, b) for a, b in all_base_forms if a != b]
+
+def replace_keys(replacements: List[Tuple[str, str]], dic: Dict[str, List[str]]) -> Dict[str, List[str]]:
+    for a, b in replacements:
+        if a in dic.keys():
+            if b in dic.keys():
+                dic[b] = list(set(dic[a] + copy(dic[b])))
+            else:
+                dic[b] = dic[a]
+            del dic[a]
+        else:
+            pass
+            # key consisting of multiple words TODO
+    return dic
 
 
 if __name__ == "__main__":
