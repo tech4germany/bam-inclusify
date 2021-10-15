7e5f31cdc40ae77a8105b3207546977b01665351
David Pomerenke <david.pomerenke@uni-konstanz.de>
2021-10-15 18:12:17 +0200

    Basic match return

--- server/matches.py
+++ server/matches.py
@@ -9,7 +9,7 @@ rules = csvs_to_dict("../data/unified")["sg"]
 
 
 def matches(text: str):
-    return list(map(mock_match, gender_matches(text)))
+    return [mock_match(t, gender_matches(t)) for t in tokens(text)]
 
 
 def gender_matches(text: str):
@@ -27,33 +27,28 @@ def token_replacements(token):
         return []
 
 
-def mock_match(replacements: List[str]):
+def mock_match(text: str, replacements: List[str]):
     replacement_values = list(map(lambda a: {"value": a}, replacements))
     return {
-    "message": "Evtl. passt der geschlechtsneutrale Begriff „Reinigungskraft“ besser.",
-    "shortMessage": "",
-    "replacements": replacement_values,
-    "offset": 13,
-    "length": 8,
-    "context": {
-        "text": "Ich bin eine Putzfrau.",
+        "message": message,
+        "shortMessage": short_message.format(text),
+        "replacements": replacement_values,
         "offset": 13,
         "length": 8,
-    },
-    "sentence": "Ich bin eine Putzfrau.",
-    "type": {"typeName": "Hint"},
-    "rule": {
-        "id": "PUTZFRAU",
-        "subId": "1",
-        "sourceFile": "grammar.xml",
-        "description": "'Reinigungskraft' statt 'Putzfrau'",
-        "issueType": "style",
-        "category": {
-            "id": "GENERISCHES_MASKULINUM",
-            "name": "Geschlechtergerechte Sprache",
+        "context": {
+            "text": text,
+            "offset": 0,
+            "length": len(text),
         },
-        "isPremium": False,
-    },
-    "ignoreForIncompleteSentence": False,
-    "contextForSureMatch": 0,
-}
+        "rule": {
+            "category": {
+                "id": "GENERISCHES_MASKULINUM",
+                "name": "Generisches Maskulinum",
+            },
+        },
+    }
+
+
+short_message = 'Die Bezeichnung "{}" spricht nur männliche Leser an. Versuche alle Menschen anzusprechen.'
+
+message = "Der Stern wird in den letzten Jahren zunehmend verwendet. Besonders häufig findet man das Sternchen im Kontexten, in denen aufgrund aktuelle Transgender- und Intersexualitätsdebatten nicht von lediglich zwei Geschlechtern ausgegangen wird, Geschlecht also nicht mehr als ein binäre System verstanden wird. Mit dem Sternchen soll bewusst irritiert und die Möglichkeit weitere Kategorien angedeutet werden."
--- server/matches_test.py
+++
@@ -1,4 +0,0 @@
-import matches as m
-
-def test_token_replacement():
-    assert m.token_replacement("Putzfrau") == []
