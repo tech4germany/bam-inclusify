458825ada7b8e70d059b904138d92bf016f4bee9
Patrick Lehner <lehner.pa@gmail.com>
2021-10-15 17:02:23 +0200

    Move top button group for addin to separate file

---
+++ react-ui/src/office-taskpane/AddinTopButtonGroup.tsx
@@ -0,0 +1,64 @@
+import { FC } from "react";
+import styled from "styled-components";
+import { UserSettingsButton, AddinCheckTextButton } from "../common/buttons/Buttons";
+import { Colors } from "../common/Colors";
+import { FontFamilies } from "../common/Fonts";
+
+interface AddinButtonGroupProps {
+  onCheckClicked: () => void;
+  settingsOpenState: [boolean, React.Dispatch<React.SetStateAction<boolean>>];
+}
+export const AddinTopButtonGroup: FC<AddinButtonGroupProps> = ({ onCheckClicked, settingsOpenState }) => (
+  <AddinButtonGroupContainer>
+    <InclusifyLogoLinkTile />
+    <UserSettingsButton pressedState={settingsOpenState} />
+    <AddinCheckTextButton onClick={onCheckClicked} />
+  </AddinButtonGroupContainer>
+);
+const AddinButtonGroupContainer = styled.div`
+  display: flex;
+  gap: 10px;
+  justify-content: flex-end;
+  margin-bottom: 8px;
+`;
+
+const InclusifyLogoLinkTile = () => (
+  <InclusifyLogoLinkTileContainer href="#">
+    <InclusifyLogoContainer />
+    <InclusifyLogoLinkText>Deine Assistentin für diversitätsensible Sprache</InclusifyLogoLinkText>
+  </InclusifyLogoLinkTileContainer>
+);
+const InclusifyLogoLinkTileContainer = styled.a`
+  background: #ffffff 0% 0% no-repeat padding-box;
+  box-shadow: 0px 3px 6px #00000029;
+  border-radius: 8px;
+  min-width: 145px;
+  min-height: 95px;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  padding: 20px 20px 15px;
+  align-items: center;
+  justify-content: center;
+  box-sizing: border-box;
+  text-decoration: none;
+  color: ${Colors.darkBlueText};
+
+  &:hover {
+  }
+`;
+const InclusifyLogoContainer = styled.div`
+  border: 1px solid gray;
+  width: 14px;
+  height: 26px;
+  box-sizing: border-box;
+`;
+const InclusifyLogoLinkText = styled.div`
+  font-family: ${FontFamilies.bam};
+  font-size: 8px;
+  line-height: 9px;
+  font-style: italic;
+  font-weight: 400;
+  max-width: 100px;
+  text-align: center;
+`;
--- react-ui/src/office-taskpane/TaskpaneApp.tsx
+++ react-ui/src/office-taskpane/TaskpaneApp.tsx
@@ -5,7 +5,6 @@ import { ApplyReplacementFunction, ResultsArea } from "../common/results-display
 import { LanguageToolClient } from "../common/language-tool-api/LanguageToolClient";
 import { isRunningInOutlook, isRunningInWord } from "../common/office-api-helpers";
 import { splitTextMatch } from "../common/splitTextMatch";
-import { AddinCheckTextButton, UserSettingsButton } from "../common/buttons/Buttons";
 import { computeErrorCounts, SummaryBar } from "../common/summary-bar/SummaryBar";
 import {
   UserSettingsContext,
@@ -16,8 +15,7 @@ import { FeatureFlagsContext, FeatureFlagsStorage, useFeatureFlagsState } from "
 import { DebugPanel } from "../common/debug-panel/DebugPanel";
 import { UserSettingsPanel } from "../common/user-settings/UserSettingsPanel";
 import { isFunction } from "../common/type-helpers";
-import { FontFamilies } from "../common/Fonts";
-import { Colors } from "../common/Colors";
+import { AddinTopButtonGroup } from "./AddinTopButtonGroup";
 
 export const TaskpaneApp: FC = () => {
   const [ltMatches, setLtMatches] = useState<RuleMatch[]>([]);
@@ -40,7 +38,7 @@ export const TaskpaneApp: FC = () => {
     <AddinContainer>
       <UserSettingsContext.Provider value={userSettings}>
         <FeatureFlagsContext.Provider value={featureFlags}>
-          <AddinButtonGroup
+          <AddinTopButtonGroup
             onCheckClicked={checkTextWithLoading}
             settingsOpenState={[isSettingsOpen, setSettingsOpen]}
           />
@@ -80,65 +78,6 @@ export const TaskpaneApp: FC = () => {
 
 const AddinContainer = styled.div`
   margin: 0 15px;
-`;
-
-interface AddinButtonGroupProps {
-  onCheckClicked: () => void;
-  settingsOpenState: [boolean, React.Dispatch<React.SetStateAction<boolean>>];
-}
-const AddinButtonGroup: FC<AddinButtonGroupProps> = ({ onCheckClicked, settingsOpenState }) => (
-  <AddinButtonGroupContainer>
-    <InclusifyLogoLinkTile />
-    <UserSettingsButton pressedState={settingsOpenState} />
-    <AddinCheckTextButton onClick={onCheckClicked} />
-  </AddinButtonGroupContainer>
-);
-const AddinButtonGroupContainer = styled.div`
-  display: flex;
-  gap: 10px;
-  justify-content: flex-end;
-  margin-bottom: 8px;
-`;
-
-const InclusifyLogoLinkTile = () => (
-  <InclusifyLogoLinkTileContainer href="#">
-    <InclusifyLogoContainer />
-    <InclusifyLogoLinkText>Deine Assistentin für diversitätsensible Sprache</InclusifyLogoLinkText>
-  </InclusifyLogoLinkTileContainer>
-);
-const InclusifyLogoLinkTileContainer = styled.a`
-  background: #ffffff 0% 0% no-repeat padding-box;
-  box-shadow: 0px 3px 6px #00000029;
-  border-radius: 8px;
-  min-width: 145px;
-  min-height: 95px;
-  display: flex;
-  flex-direction: column;
-  gap: 16px;
-  padding: 20px 20px 15px;
-  align-items: center;
-  justify-content: center;
-  box-sizing: border-box;
-  text-decoration: none;
-  color: ${Colors.darkBlueText};
-
-  &:hover {
-  }
-`;
-const InclusifyLogoContainer = styled.div`
-  border: 1px solid gray;
-  width: 14px;
-  height: 26px;
-  box-sizing: border-box;
-`;
-const InclusifyLogoLinkText = styled.div`
-  font-family: ${FontFamilies.bam};
-  font-size: 8px;
-  line-height: 9px;
-  font-style: italic;
-  font-weight: 400;
-  max-width: 100px;
-  text-align: center;
 `;
 
 const SummaryBarContainer = styled.div`
