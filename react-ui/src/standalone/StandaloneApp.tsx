79671210e2d682eb790e6de7695fe5917802b451
Patrick Lehner <lehner.pa@gmail.com>
2021-10-18 13:46:03 +0200

    Move WelcomeMessage to separate file for re-use

--- react-ui/src/standalone/StandaloneApp.tsx
+++ react-ui/src/standalone/StandaloneApp.tsx
@@ -16,6 +16,7 @@ import { MainTextArea } from "./MainTextArea";
 import { StandaloneUserSettingsButton } from "./StandaloneUserSettingsButton";
 import { Fonts } from "../common/styles/Fonts";
 import { CenteredContainer } from "./CenteredContainer";
+import { WelcomeMessage } from "../common/WelcomeMessage";
 
 type UseState<S> = [S, Dispatch<SetStateAction<S>>];
 
@@ -162,31 +163,3 @@ const ButtonBar = styled.div`
 const ButtonBarSpacer = styled.div`
   flex-grow: 1;
 `;
-
-const WelcomeMessageContainer = styled.div`
-  font-family: ${Fonts.bam.family};
-  font-size: 20px;
-  font-style: italic;
-`;
-
-const WelcomeMessageIntro = styled.p`
-  font-weight: ${Fonts.bam.weights.bold};
-  margin: 0;
-`;
-const WelcomeMessageBody = styled.p`
-  margin: 1em 0 0;
-  font-weight: ${Fonts.bam.weights.normal};
-`;
-
-const WelcomeMessage = () => (
-  <WelcomeMessageContainer>
-    <WelcomeMessageIntro>
-      Herzlich Willkommen bei INCLUSIFY, deiner Assistentin für diversitätssensible Sprache bei der BAM.
-    </WelcomeMessageIntro>
-    <WelcomeMessageBody>
-      Prüfe Deine Texte auf Diversitätslücken. INCLUSIFY ist aktuell basierend auf den Präferenzen des BAM Leitfadens
-      für geschlechtersensible Sprache eingestellt. Wenn Du eine anderes Gendersprache bevorzugst, kannst Du das in den
-      Einstellungen anpassen.
-    </WelcomeMessageBody>
-  </WelcomeMessageContainer>
-);
