7b4fda7afd2009c20f88689ffdbd0ce649399ad6
Patrick Lehner <lehner.pa@gmail.com>
2021-10-20 13:39:11 +0200

    Clean up build-zip-file devcmd

--- dev_cmds/build-zip-file.ts
+++ dev_cmds/build-zip-file.ts
@@ -1,16 +1,15 @@
 import { execPiped, runAsyncMain } from "devcmd";
 import fs from "fs-extra";
-import { cyan, red } from "kleur";
+import { cyan } from "kleur";
 import path from "path";
-import { cwd } from "process";
 import { DEVCMD_COMMAND } from "./utils/commands";
-import { dataDir, languageToolDir, reactUiDir, repoRoot } from "./utils/paths";
+import { dataDir, reactUiDir, repoRoot, serverDir } from "./utils/paths";
 
 async function main() {
-  // await execPiped({ command: DEVCMD_COMMAND, args: ["build-react-app"] });
+  await execPiped({ command: DEVCMD_COMMAND, args: ["build-react-app"] });
 
-  const staticFileDir = path.join(repoRoot, "inclusify_server/static");
-  const serverDataDir = path.join(repoRoot, "inclusify_server/data");
+  const staticFileDir = path.join(serverDir, "static");
+  const serverDataDir = path.join(serverDir, "data");
 
   await fs.remove(staticFileDir);
   await fs.mkdirp(staticFileDir);
@@ -18,21 +17,20 @@ async function main() {
 
   await fs.copy(path.join(dataDir, "unified.csv"), path.join(serverDataDir, "unified.csv"));
 
-  await execPiped({ command: "zip", args: ["-r", "inclusify_server.zip", "inclusify_server"], options: { cwd: repoRoot } });
+  const serverDirName = path.relative(repoRoot, serverDir);
+  const zipFileName = `${serverDirName}.zip`;
+  const zipFilePath = path.join(repoRoot, zipFileName);
 
+  await fs.remove(zipFilePath);
+  await execPiped({
+    command: "zip",
+    args: ["-r", zipFileName, serverDirName],
+    options: { cwd: repoRoot },
+  });
 
-  const zipFiles = (await fs.readdir(repoRoot, { encoding: "utf-8" })).filter((filename) =>
-    filename.toLowerCase().endsWith(".zip")
-  );
-  if (zipFiles.length === 1) {
-    console.log();
-    console.log("Your ZIP file was built and is available here:");
-    console.log("  " + cyan(path.join(repoRoot, zipFiles[0])));
-  } else {
-    console.error(
-      red(`Error: Expected 1 ZIP file in output dir '${repoRoot}' but found ${zipFiles.length}`)
-    );
-  }
+  console.log();
+  console.log("Your ZIP file was built and is available here:");
+  console.log("  " + cyan(zipFilePath));
 }
 
 runAsyncMain(main);
--- dev_cmds/utils/paths.ts
+++ dev_cmds/utils/paths.ts
@@ -5,5 +5,6 @@ const repoRoot = resolve(__dirname, "..", "..");
 const dataDir = resolve(repoRoot, "data");
 const languageToolDir = resolve(repoRoot, "languagetool");
 const reactUiDir = resolve(repoRoot, "react-ui");
+const serverDir = resolve(repoRoot, "inclusify_server");
 
-export { repoRoot, dataDir, languageToolDir, reactUiDir };
+export { repoRoot, dataDir, languageToolDir, reactUiDir, serverDir };
