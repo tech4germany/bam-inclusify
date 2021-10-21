a50c054f7be714f2807148a84d63325f8a187d40
Patrick Lehner <lehner.pa@gmail.com>
2021-10-21 16:54:46 +0200

    Improve behavior of ExpandCollapse

--- react-ui/src/common/results-display/ExpandCollapse.tsx
+++ react-ui/src/common/results-display/ExpandCollapse.tsx
@@ -1,16 +1,21 @@
-import { FC, useRef, useEffect } from "react";
+import { FC, useRef, useEffect, useState } from "react";
 import styled from "styled-components";
 
 export const ExpandCollapse: FC<{ isExpanded: boolean }> = ({ isExpanded, children }) => {
   const containerRef = useRef<HTMLDivElement>(null);
   const measurerRef = useRef<HTMLDivElement>(null);
   const heightRef = useRef(0);
+  const [computedHeight, setComputedHeight] = useState<number>();
   useEffect(() => {
     heightRef.current = measurerRef.current?.scrollHeight || 0;
+    setComputedHeight(measurerRef.current?.scrollHeight || 0);
   });
 
   return (
-    <ExpandCollapseContainer ref={containerRef} style={{ height: isExpanded ? heightRef.current + "px" : "0" }}>
+    <ExpandCollapseContainer
+      ref={containerRef}
+      style={{ height: !isExpanded ? "0" : typeof computedHeight === "number" ? computedHeight + "px" : "auto" }}
+    >
       <ExpandCollapseMeasurer ref={measurerRef}>{children}</ExpandCollapseMeasurer>
     </ExpandCollapseContainer>
   );
