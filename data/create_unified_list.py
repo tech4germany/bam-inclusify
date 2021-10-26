3f3ce0946593e92577e978f49c93f587ae8f93a1
David Pomerenke <david.pomerenke@uni-konstanz.de>
2021-10-26 11:52:58 +0200

    Simpler intermediate list

--- data/create_unified_list.py
+++ data/create_unified_list.py
@@ -1,16 +1,7 @@
-from copy import copy
-from helpers import open_, add_to_dict
+from helpers import open_
 from os import path
 from typing import *
 import csv
-import itertools
-import re
-import stanza
-import subprocess
-
-# stanza.download("de")
-
-nlp = stanza.Pipeline(lang="de", processors="tokenize,mwt,pos,lemma,depparse")
 
 
 def unified_list():
@@ -19,31 +10,11 @@ def unified_list():
         ("geschicktgendern", "geschicktgendern/geschicktgendern_normalized.csv"),
         # "vienna_catalog/vienna_catalog.csv",
     ]
-    dic = {}
+    rows = []
     for name, file in files:
         rules = list(csv.reader(open_(file)))
-        for lemma, rules in lemmatize_rules(rules, name).items():
-            add_to_dict(lemma, rules, dic)
-    rows = []
-    for lemma, rules in dic.items():
-        for y in rules:
-            (insensitive_lemmas, insensitive, sensitive, plural_only, source) = y
-            rows.append([lemma, insensitive_lemmas, insensitive, sensitive, plural_only, source])
-    csv.writer(open("unified.csv", "w")).writerows(rows)
-
-
-def lemmatize_rules(rules, source):
-    # save the lemma of the root of the insensitive part of each rule for easy matching
-    lemmatized_rules = {}
-    for [insensitive, sensitive, plural_only] in rules:
-        doc = nlp(insensitive)
-        insensitive_lemmas = ";".join(list(itertools.chain(*[[word.lemma for word in sentence.words] for sentence in nlp(insensitive).sentences])))
-        for sentence in doc.sentences:
-            for word in sentence.words:
-                if word.head == 0:
-                    add_to_dict(
-                        word.lemma, [(insensitive_lemmas, insensitive, sensitive, plural_only, source)], lemmatized_rules)
-    return lemmatized_rules
+        rows += [(a, b, c, name) for a, b, c in rules]
+    csv.writer(open_("unified.csv", "w")).writerows(sorted(rows))
 
 
 if __name__ == "__main__":
