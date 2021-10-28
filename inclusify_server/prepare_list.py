f588fb3d249ab653f6e48a86862f04cd6846d566
David Pomerenke <david.pomerenke@uni-konstanz.de>
2021-10-28 11:01:19 +0200

    Change order for "Mitarbeiter"

--- inclusify_server/prepare_list.py
+++ inclusify_server/prepare_list.py
@@ -11,7 +11,8 @@ def preprocess_rules():
     print("Looking for rule changes in `data/unified.csv`.")
     rules = read_rule_file("unified.csv")
     old_rules = read_rule_file("unified.csv.old")
-    processed_rules = list(csv.reader(open_(path.join("data", "processed.csv"))))
+    processed_rules = list(csv.reader(
+        open_(path.join("data", "processed.csv"))))
     print("Removing old rules ...")
     for insensitive, sensitive, plural_only, source in tqdm(
         old_rules.difference(rules)
@@ -34,10 +35,10 @@ def preprocess_rules():
             processed_rules += lemmatize_rule(rule)
 
     csv.writer(open_(path.join("data", "unified.csv.old"), "w")).writerows(
-        sorted(list(rules))
+        list(rules)
     )
     csv.writer(open_(path.join("data", "processed.csv"), "w")).writerows(
-        sorted(processed_rules)
+        processed_rules
     )
     print("Rule changes have been processed.")
 
@@ -75,7 +76,8 @@ def lemmatize_rule(rule):
 
 def read_rule_file(x):
     return set(
-        [(a, b, c, d) for [a, b, c, d] in csv.reader(open_(path.join("data", x)))]
+        [(a, b, c, d)
+         for [a, b, c, d] in csv.reader(open_(path.join("data", x)))]
     )
 
 
