8226ce7d339e64c423b7f8c5db5888f6fc370602
Patrick Lehner <lehner.pa@gmail.com>
2021-11-02 13:07:16 +0100

    Allow mailto links for navbar

--- react-ui/src/common/isValidUrl.ts
+++ react-ui/src/common/isValidUrl.ts
@@ -1,3 +1,3 @@
 export function isValidUrl(value: string): boolean {
-  return value.startsWith("https://") || value.startsWith("http://");
+  return value.startsWith("https://") || value.startsWith("http://") || value.startsWith("mailto:");
 }
