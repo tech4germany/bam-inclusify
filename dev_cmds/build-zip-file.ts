2bbb1240e0d58dc4bad08562ab6ece37fe3581d7
Patrick Lehner <lehner.pa@gmail.com>
2021-11-02 13:55:27 +0100

    Update build-zip-file command to use npm package instead of native "zip" executable (to make it runnable on Windows)

--- dev_cmds/build-zip-file.ts
+++ dev_cmds/build-zip-file.ts
@@ -2,6 +2,7 @@ import { execPiped, runAsyncMain } from "devcmd";
 import fs from "fs-extra";
 import { cyan } from "kleur";
 import path from "path";
+import archiver from "archiver";
 import { DEVCMD_COMMAND } from "./utils/commands";
 import { repoRoot, serverDir } from "./utils/paths";
 
@@ -13,15 +14,32 @@ async function main() {
   const zipFilePath = path.join(repoRoot, zipFileName);
 
   await fs.remove(zipFilePath);
-  await execPiped({
-    command: "zip",
-    args: ["-r", zipFileName, serverDirName],
-    options: { cwd: repoRoot },
-  });
+
+  console.log();
+  console.log("Creating ZIP file...");
+  await createServerZip(serverDir, zipFilePath);
 
   console.log();
   console.log("Your ZIP file was built and is available here:");
   console.log("  " + cyan(zipFilePath));
+}
+
+function createServerZip(serverDir: string, zipFilePath: string) {
+  const output = fs.createWriteStream(zipFilePath);
+  const outputCompletionPromise = new Promise<void>((resolve, reject) => {
+    output.on("close", () => resolve());
+    output.on("finish", () => resolve());
+    output.on("error", (e) => reject(e));
+  });
+
+  const archive = archiver("zip");
+  archive.pipe(output);
+
+  const serverDirName = path.relative(repoRoot, serverDir);
+  archive.directory(serverDir, serverDirName);
+
+  const archiveFinalizePromise = archive.finalize();
+  return Promise.all([outputCompletionPromise, archiveFinalizePromise]);
 }
 
 runAsyncMain(main);
